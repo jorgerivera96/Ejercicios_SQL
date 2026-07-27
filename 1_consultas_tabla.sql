4d467667aa4a7d3254fa558ded71df04b95ac61e
Jorge.Rivera <jorgeriverab96@gmail.com>
2026-07-27 12:26:34 -0500

    first commit

---
+++ 1_consultas_tabla.sql
@@ -0,0 +1,14 @@
+-- 1. Lista el nombre de todos los productos que hay en la tabla producto.
+SELECT nombre
+FROM producto
+
+-- 2. Lista los nombres y los precios de todos los productos de la tabla producto.
+SELECT nombre, precio
+FROM producto
+
+-- 3. Lista todas las columnas de la tabla producto.
+SELECT *
+FROM producto
+
+-- 4. Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD).
+s asd
